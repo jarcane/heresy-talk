086e791ba8d04697655957fb7703ebf8384273b0
John Berry <john.berry@metosin.fi>
2016-10-25 11:24:21 +0200

    For loop

--- heresy-talk.rkt
+++ heresy-talk.rkt
@@ -41,3 +41,15 @@
  (item "Heresy is for learning")
  (item "Heresy is an experiment")
  (item "Heresy is for everyone"))
+
+(slide
+ #:title "For Loops"
+ (code (def fn fizzbuzz (n)
+         (for (x in (range 1 to n))
+           (select
+            ((zero? x) x)
+            ((zero? (+ (mod x 5)
+                       (mod x 3))) (print "FizzBuzz"))
+            ((zero? (mod x 5)) (print "Buzz"))
+            ((zero? (mod x 3)) (print "Fizz"))
+            (else (print x)))))))
